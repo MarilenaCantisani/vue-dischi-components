23ee2e65deedd3b7cbdf8fbcc12d68042af62ce0
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-09 15:19:22 +0200

    Main and card component with style

---
+++ src/components/Card.vue
@@ -0,0 +1,48 @@
+<template>
+  <!--* Card -->
+  <li class="card">
+    <img class="poster" src="@/assets/demo.jpg" alt="card-title" />
+    <h2 class="title text-uppercase">titolo</h2>
+    <address class="author text-capitalize text-normal">autore</address>
+    <time class="year text-grey">anno</time>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "Card",
+};
+</script>
+
+<style scoped lang="scss">
+//// Variables
+@import "../assets/scss/Generals/Style.scss";
+
+//// Card style
+.card {
+  text-align: center;
+  list-style-type: none;
+  padding: 20px;
+  width: 200px;
+  background-color: $light-blue;
+  margin: 10px 10px;
+  .poster {
+    width: 100%;
+    height: auto;
+  }
+  .title {
+    padding: 15px 0;
+    font-size: 22px;
+    color: $white;
+  }
+  .author {
+    font-size: 20px;
+    padding-top: 15px;
+    color: $light-grey;
+  }
+  .year {
+    font-size: 15px;
+    color: $light-grey;
+  }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <main>
+    <article>
+      <!-- CARD SECTION -->
+      <ul class="card-section">
+        <Card />
+      </ul>
+    </article>
+  </main>
+</template>
+
+<script>
+import Card from "../components/Card.vue";
+export default {
+  name: "Main",
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+//// Variables
+@import "../assets/scss/Generals/Style.scss";
+//// Card section style
+.card-section {
+  margin-top: 50px;
+  padding: 0 10px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
