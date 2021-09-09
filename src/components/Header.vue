152411f7ac8310eeb0a87807c0f51537c049c2a2
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-09-09 15:02:31 +0200

    Header style

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="container">
+      <!-- Logo -->
+      <img id="logo" src="@/assets/logo.png" alt="logo" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+//// Variables
+@import "../assets/scss/Generals/Style.scss";
+//// Header style
+@import "../assets/scss/Header/Header.scss";
+</style>
